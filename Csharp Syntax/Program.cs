720970d8cbf697c777416bf50fa102e63ee875ff
rmoore0805 <rmoore88499@uat.edu>
2024-06-01 13:33:05 -0500

    New project

---
+++ Csharp Syntax/Program.cs
@@ -0,0 +1,14 @@
+﻿/*multi-line comment
+ * Author: Rachel Moore
+ * * program: InClassSyntax
+ * Purpose: An overview of C# Syntax across varibales, data structure, literal. and operrators.
+  * Program: C_Sharp_Syntax_Reference
+ * Prupose: An overview of C# Syntax across variables, data structures, literals and operators.
+ * Developed for UAT MS539 Syntax Examples
+ * Guided by the C# Programming Yellow Book by Rob Miles: https://static1.squarespace.com/static/5019271be4b0807297e8f404/t/5df9306ec60881645ea57ced/1576611956760/CSharp+Book+2019+Refresh.pdf
+ * .Net Design Guidelines including naming conventions: https://learn.microsoft.com/en-us/previous-versions/dotnet/netframework-1.1/czefa0ke(v=vs.71)?redirectedfrom=MSDN
+ * Guidance on Variables: https://en.wikibooks.org/wiki/C_Sharp_Programming/Variables
+ */
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
